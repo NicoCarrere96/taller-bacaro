8911655545078f75db02e53ed4d2137d881241a5
Sebastian Basconcelo <el_basco@hotmail.com>
2019-09-19 00:25:22 -0300

    Add files via upload
    Agrego Servicios Taller

---
+++ src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioTallerImpl.java
@@ -0,0 +1,27 @@
+package ar.edu.unlam.tallerweb1.servicios;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import ar.edu.unlam.tallerweb1.dao.TallerDao;
+import ar.edu.unlam.tallerweb1.modelo.taller.Taller;
+@Service("servicioTaller")
+@Transactional
+public class ServicioTallerImpl implements ServicioTaller{
+	
+	@Inject
+	private TallerDao servicioTallerDao;
+	public Taller consultarTaller(Taller taller) {
+		return servicioTallerDao.consultarTaller(taller);
+	}
+	
+	
+	public void crearTaller(Taller taller) {
+		servicioTallerDao.crearTaller(taller);
+		
+	}
+
+
+}
